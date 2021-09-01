3e8b492cf7b46d39e194d458d1c8010b4b745298
Avigail12 <Avigailcohen1111@gmail.com>
2021-09-01 15:44:48 +0300

    add validations

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,73 +1,75 @@
 var fs = require('fs').promises;
+var fs2 = require('fs');
 // var fs = require('fs');
 var parse = require('csv-parse/lib/sync');
 const axios = require('axios');
 const stringify = require('csv-stringify');
-
+const ObjectsToCsv = require('objects-to-csv')
 
 async function createManyMessages(req, res) {
 
         if(!req.body.source_file_location){
           return res.status(400).json({ status: "fail", message: `please provide a source file location` })
         }   
-        // if (!fs.existsSync(req.body.source_file_location)){
-        //     return res.status(404).json({ status: "fail", message: `source file location not exists` })
-        // }
+        if (!fs2.existsSync(req.body.source_file_location)){
+            return res.status(404).json({ status: "fail", message: `source file location not exists` })
+        }
         try {
+
             var result = await readFile(req.body.source_file_location)
-            await insertAllMessages(result,req.body.source_file_location)
+
+            //check if illigel
+            var columnResults = {};
+
+            for(var row =0; row < result.length; row++){
+                for(var column in result[row]){
+                    if(!columnResults[column]){
+                        columnResults[column] = [];
+                    }
+                    columnResults[column].push(result[row][column]);
+                }
+            }
+            columns = Object.keys(columnResults);
+            if(columns[0] != "from_name" | columns[1] != "to_name" | columns[2] != "message"| columns[3] != "date" | columns[4] != "key"){
+                return res.status(400).json({ status: "fail", message: 'The colums name wrong' })
+            }
+            console.log('yyyyyyy');
+            resp = await insertAllMessages(result,req.body.source_file_location)
         } catch (error) {
             return res.status(400).json({ status: "fail", message: error.message })
-            console.log('erorr');
         }
         console.log('after');
-       return res.status(200).json({ status: "ok", payload: "" })
+       return res.status(200).json({ status: "ok", payload: resp })
   }
 
-  async function insertAllMessages(files,path){
-    let columns = {
-        ID: 'id',
-        FROM_NAME: 'from_name',
-        TO_NAME:'to_name',
-        MESSAGE: 'message',
-        CREATED_AT: 'created_at',
-        UPDATED_AT: 'updated_at',
-        KEY: 'key',
-      };
+  async function insertAllMessages(rows,path){
+
+      var data = []
 
     try {
-           files.forEach(file => {
-            axios.post('http://localhost:5000/api/messages',file)
-            .then(response => {
-                console.log(response.data.payload);
-                // stringify(response.data.payload, { header: true, columns: columns }, function (err, output) {
-                //     if (err) throw err;
-                //     console.log('write');
-                //     fs.writeFile(path, output,(err) => {
-                //         if (err) throw err;
-                //         console.log('my.csv saved.');
-                //       });
-                // })
-                  fs.writeFile(path, response.data.payload, 'utf8', function (err) {
-                    if (err) {
-                      console.log('Some error occured - file either not saved or corrupted file saved.');
-                    } else{
-                      console.log('It\'s saved!');
-                    }
-                  });
-                return;
-            })
-            .catch(error => {
-                  throw error
-                // console.log(error.response.data.message);
-                // return error;
-            });
-        });
+            // rows.forEach(row => {
+            //     result = await axios.post('http://localhost:5000/api/messages',row)
+            // })
+            for (let index = 0; index < rows.length; index++) {
+                 result = await axios.post('http://localhost:5000/api/messages',rows[index])
+                 message = {from_name:result.data.payload.FROM_NAME,to_name:result.data.payload.TO_NAME,
+                    message:result.data.payload.MESSAGE,date:result.data.payload.CREATED_AT,key:result.data.payload.KEY}
+                    d = new Date(message.date)
+                    message.date = d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear()
+                 data.push(message) 
+            }
+            console.log(data);
+             csv = new ObjectsToCsv(data)
+             await csv.toDisk(path)
+
+        // return data;
     } catch (error) {
         throw error
     }
   }
 
+
+  // read all messages from file and convers to objects array
   async function readFile(path){
 
     try {
@@ -78,8 +80,6 @@ async function createManyMessages(req, res) {
         throw error;
     }
   }
-
-
 
   module.exports = {
     createManyMessages,
